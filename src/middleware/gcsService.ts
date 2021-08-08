1c902697dab0f89945757f783ec0e35c6cd3deb3
michaeljwright <mike.filmworks@gmail.com>
2021-08-08 21:34:32 +0100

    Added merge method

--- src/middleware/gcsService.ts
+++ src/middleware/gcsService.ts
@@ -1,6 +1,9 @@
 import { Bucket, Storage } from "@google-cloud/storage";
+import { PDFDocument } from "pdf-lib";
 import multer from "multer";
 import path from "path";
+import fetch from "cross-fetch";
+import fs from "fs";
 
 import GcsUpload from "./gcsUpload";
 
@@ -8,6 +11,12 @@ type constructorOptions = {
   projectId: string;
   keyFilename: string;
   bucketName: string;
+};
+
+type mergeOptions = {
+  folder: string;
+  entityUid: string;
+  luxId: string;
 };
 
 class GoogleCloudStorageService {
@@ -45,8 +54,53 @@ class GoogleCloudStorageService {
     return fileList;
   };
 
-  mergeFiles = async (mergeOptions: any) => {
-    return mergeOptions;
+  mergeFiles = async (opts: mergeOptions) => {
+    let saveLocally: boolean = false;
+    let fileList: { name: string; created: Date; updated: Date }[] = [];
+    const mergedPdf = await PDFDocument.create();
+    const [files] = await this.bucket.getFiles({ prefix: opts.folder });
+
+    console.log(`entityUid: ${opts.entityUid}`);
+
+    for (let file of files) {
+      if (file.name.includes(opts.luxId)) {
+        fileList.push({
+          name: file.name,
+          created: file.metadata.timeCreated,
+          updated: file.metadata.updated
+        });
+
+        const [url] = await this.bucket.file(file.name).getSignedUrl({
+          version: "v2",
+          action: "read",
+          expires: Date.now() + 1000 * 60 * 9 // 9 minutes
+        }); //generate signed url
+
+        const arrayBuffer: ArrayBuffer = await fetch(url).then(res =>
+          res.arrayBuffer()
+        );
+
+        const pdf = await PDFDocument.load(arrayBuffer);
+
+        for (let idx of pdf.getPageIndices()) {
+          const [copiedPage] = await mergedPdf.copyPages(pdf, [idx]);
+          mergedPdf.addPage(copiedPage);
+        }
+      }
+    }
+
+    const mergedPdfFile = await mergedPdf.save(); // { addDefaultPage: false }
+    const mergedPdfFileName = `mergedfile_${Date.now()}_${opts.luxId}.pdf`;
+
+    if (saveLocally) {
+      fs.writeFileSync(`temp/${mergedPdfFileName}`, Buffer.from(mergedPdfFile));
+    }
+
+    // save to GCP bucket
+    const outputFile = this.bucket.file(mergedPdfFileName);
+    await outputFile.save(Buffer.from(mergedPdfFile));
+
+    return fileList;
   };
 
   uploadMiddleware = () => {
